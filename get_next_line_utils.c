9b20aecc7ba56ae0b1b4ac81786865ee37ed5f52
ByteGab <gabrielfreiresimon@gmail.com>
2024-11-26 17:52:43 +0100

    add get_next_line_utils.c

---
+++ get_next_line_utils.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 16:52:11 by gafreire          #+#    #+#             */
+/*   Updated: 2024/11/19 18:13:59 by gafreire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h" 
+
+char *ft_strjoin (char const *s1, char const *s2)
+{
+    
+}
+char	*ft_strdup (const char *s)
+{
+    
+}
+
+size_t	ft_strlen(const char *s)
+{
+    
+}
+ft_strchr
+ftsubstr 
