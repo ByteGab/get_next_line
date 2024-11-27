8965d472a7ad8a8ad5411e5e7835261387b07d52
Bytegab <gabrielfreiresimon@gmail.com>
2024-11-27 03:15:50 +0100

    change get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,24 +6,60 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 16:52:11 by gafreire          #+#    #+#             */
-/*   Updated: 2024/11/19 18:13:59 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/11/27 02:28:29 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h" 
+#include "get_next_line.h"
 
-char *ft_strjoin (char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    
+	size_t	size;
+	char	*mem;
+
+	size = ft_strlen(s1) + ft_strlen(s2);
+	mem = (char *)malloc(size + 1);
+	if (mem == NULL)
+		return (NULL);
+	ft_strlcpy(mem, s1, ft_strlen(s1) + 1);
+	ft_strlcat(mem, s2, size + 1);
+	return (mem);
 }
-char	*ft_strdup (const char *s)
+char	*ft_strdup(const char *s)
 {
-    
+	char	*mem;
+	size_t	size;
+
+	size = ft_strlen(s) + 1;
+	mem = (char *)malloc(size);
+	if (mem != NULL)
+		ft_strlcpy(mem, s, size);
+	return (mem);
 }
 
 size_t	ft_strlen(const char *s)
 {
-    
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
-ft_strchr
-ftsubstr 
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((unsigned char)c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
