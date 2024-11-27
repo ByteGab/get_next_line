b47267d7960cc770e8f53f480a8bc8d6446861ef
Bytegab <gabrielfreiresimon@gmail.com>
2024-11-27 03:15:18 +0100

    change get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,22 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 17:19:28 by gafreire          #+#    #+#             */
-/*   Updated: 2024/11/19 18:43:00 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/11/27 02:21:16 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 18
+# endif
+
 # include <unistd.h>
 
-int get_next_line (int fd);
-char *ft_strjoin (char const *s1, char const *s2);
-char	*ft_strdup (const char *s);
-size_t	ft_strlen(const char *s); 
-    
+int		get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+
 #endif
