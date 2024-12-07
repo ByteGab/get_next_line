b7c01a06ef2b0175e8bb92c3984f44034d6498ae
ByteGab <gabrielfreiresimon@gmail.com>
2024-12-07 20:54:19 +0100

    change define

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 17:19:28 by gafreire          #+#    #+#             */
-/*   Updated: 2024/11/27 21:47:17 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/12/02 18:48:56 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 18
@@ -24,5 +24,6 @@ int		get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
 
 #endif
