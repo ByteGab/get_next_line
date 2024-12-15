eba7e6ac4c4322237357f7258d5b78a5e331bf7e
ByteGab <gabrielfreiresimon@gmail.com>
2024-12-15 20:33:06 +0100

    add functions

--- get_next_line.c
+++ get_next_line.c
@@ -6,59 +6,118 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 16:52:08 by gafreire          #+#    #+#             */
-/*   Updated: 2024/12/14 19:01:02 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/12/15 20:31:05 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *line(char *buffer, int fd)
+char	*stash_filling(int fd, char *stash, char *buffer)
 {
-    size_t  i;
-    int     z;
-    char    *linea;
-    
-    z = 0;
-    i = read(fd,buffer, BUFFER_SIZE);
-    if (i <= 0)
-    {
-        return(NULL);
-    }
-    while(buffer[z] != '\0' && buffer[z] != '\n')
-    {
-        linea[z] = buffer[z];
-        z++;
-    }
-    buffer = ft_strlen(buffer) - ft_strlen(linea);
-}
-char    *read_file()
-{
-    
-}
-char    *left_lines()
-{
-    
+	int	size;
+
+	size = 1;
+	if (!stash)
+		stash = ft_strdup("");
+	while (!ft_strchr(stash, '\n') && size > 0)
+	{
+		size = read(fd, buffer, BUFFER_SIZE);
+		if (size == -1)
+		{
+			free (buffer);
+			return (NULL);
+		}
+		buffer[size] = '\0';
+		stash = ft_strjoin (stash, buffer);
+	}
+	free (buffer);
+	return (stash);
 }
 
-char get_next_line(int fd)
+char	*extract_new_stash(char	*stash)
 {
-    char    *line;
-    char    *box_lines;
-    
-    if (BUFFER_SIZE <= 0 || fd < 0)
+	int		len;
+	int		i;
+	char	*new_stash;
+
+	len = 0;
+	i = 0;
+	if (stash == NULL)
 		return (NULL);
-    // box_lines = read_file;
-    if (!box_lines)
-        return(NULL);
-    // line = save_line;
-    // box_lines = save_lines;
-    return (line);
+	while (stash[len] != '\n' && stash[len])
+		len++;
+	if (stash[len] == '\n')
+		len++;
+	new_stash = malloc((ft_strlen(stash) - len + 1) * sizeof(char));
+	if (!new_stash)
+		return (NULL);
+	while (stash[len + i])
+	{
+		new_stash[i] = stash[len + i];
+		i++;
+	}
+	free (stash);
+	new_stash[i] = 0;
+	return (new_stash);
 }
 
-// int main ()
-// {
-//     // // open
-//     // open("",O_READ)
-//     // // read
-//     // ssize_t read(int fd, void *buf,size_t count);
-// }
+char	*extract_line(char *stash, char *line)
+{
+	int	len;
+	int	i;
+
+	len = 0;
+	i = 0;
+	if (stash == NULL)
+		return (NULL);
+	while (stash[len] != '\n' && stash[len])
+		len++;
+	if (stash[len] == '\n')
+		len++;
+	line = malloc((len + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	while (i < len)
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	line[i] = 0;
+	return (line);
+}
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+	char		*buffer;
+
+	line = NULL;
+	buffer = (char *)malloc((BUFFER_SIZE + 1));
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(stash);
+		free(buffer);
+		stash = NULL;
+		buffer = NULL;
+		return (NULL);
+	}
+	if (!buffer)
+		return (NULL);
+	stash = stash_filling(fd, stash, buffer);
+	if (*stash == 0)
+	{
+		free (stash);
+		return (stash = 0);
+	}
+	line = extract_line(stash, line);
+	stash = extract_new_stash(stash);
+	return (line);
+}
+/*int main(void)
+{
+	int	fd;
+
+	fd = open("text.txt", O_RDONLY);
+	printf("GNL 9: %s", get_next_line(fd));
+	close(fd);
+} */
