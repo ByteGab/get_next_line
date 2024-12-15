420376907758ad428792ae32f5a93fb77671c1a0
ByteGab <gabrielfreiresimon@gmail.com>
2024-12-15 20:33:49 +0100

    add functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 17:19:28 by gafreire          #+#    #+#             */
-/*   Updated: 2024/12/02 18:48:56 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/12/15 19:04:12 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,17 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
 
-int		get_next_line(int fd);
+char	*get_next_line(int fd);
+char	*stash_filling(int fd, char *stash, char *buffer);
+char	*extract_line(char *stash, char *line);
+char	*extract_new_stash(char *stash);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s);
-size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
 
 #endif
