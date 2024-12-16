6b2e2121c6b300ac3ed6d431153a7e86497ab58e
ByteGab <gabrielfreiresimon@gmail.com>
2024-12-16 14:57:08 +0100

    change functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 17:19:28 by gafreire          #+#    #+#             */
-/*   Updated: 2024/12/15 19:04:12 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/12/16 14:40:06 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 18
+#  define BUFFER_SIZE 10
 # endif
 
 # include <stdlib.h>
@@ -24,12 +24,11 @@
 
 char	*get_next_line(int fd);
 char	*stash_filling(int fd, char *stash, char *buffer);
-char	*extract_line(char *stash, char *line);
+char	*extract_line(char *stash);
 char	*extract_new_stash(char *stash);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char *s1);
+size_t	ft_strlen(char *s);
 
 #endif
